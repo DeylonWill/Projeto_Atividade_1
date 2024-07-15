1722589868c57d10b484ee6afb5d6a6c55dcdaba
Deylon <deylonsaraiva@gmail.com>
2024-07-15 05:38:00 -0300

    Tela de itens vendidos

--- LeiloesTDSat/LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/LeiloesTDSat/src/listagemVIEW.java
@@ -143,16 +143,15 @@ public class listagemVIEW extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Por favor, preencha o campo.", "Erro", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
+        ProdutosDAO produtosdao = new ProdutosDAO();       
         produtosdao.venderProduto(Integer.parseInt(id));
         listarProdutos();
+        
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
+        vendasVIEW vendas = new vendasVIEW(); 
+        vendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
